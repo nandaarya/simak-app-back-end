41493065d09be82951ec86a3fbc1f7d68ed36358
Nanda Arya Putra <nandaarya404@gmail.com>
2023-05-24 20:56:59 +0700

    fix handler for mongoDB

--- src/handler/getAllClassesHandler.js
+++ src/handler/getAllClassesHandler.js
@@ -1,10 +1,37 @@
-const classes = require('../classes');
+// const classes = require('../classes');
+const Class = require('../models/classes');
 
-const getAllClassesHandler = () => ({
-  status: 'success',
-  data: {
-    classes,
-  },
-});
+// const getAllClassesHandler = () => ({
+//   status: 'success',
+//   data: {
+//     classes,
+//   },
+// });
+
+// try {
+//   const books = await Book.find();
+//   return h.response(books).code(200);
+// } catch (error) {
+//   console.log('err', error);
+//   return h.response({ Error: 'Something went wrong.' }).code(500);
+// }
+
+const getAllClassesHandler = async () => {
+  try {
+    const classes = await Class.find();
+    return {
+      status: 'success',
+      data: {
+        classes,
+      },
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      status: 'error',
+      message: 'Something went wrong.',
+    };
+  }
+};
 
 module.exports = { getAllClassesHandler };
