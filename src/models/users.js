dbe8461393f4e6697c4138bf5641ca08c4973911
Nanda Arya Putra <nandaarya404@gmail.com>
2023-06-04 17:34:45 +0700

    add email

--- src/models/users.js
+++ src/models/users.js
@@ -10,6 +10,10 @@ const UserSchema = new Schema({
     type: String,
     required: true,
   },
+  email: {
+    type: String,
+    require: true,
+  },
   nama: {
     type: String,
     required: true,
