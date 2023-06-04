1d5ba596974c7d98b83a07a8d35eae3652bc4f4f
Nanda Arya Putra <nandaarya404@gmail.com>
2023-06-04 21:23:34 +0700

    add login

--- src/handler/loginHandler.js
+++ src/handler/loginHandler.js
@@ -1,3 +1,5 @@
+const User = require('../models/users');
+
 const loginHandler = async (request, h) => {
   const { username, password } = request.payload;
 
@@ -9,28 +11,31 @@ const loginHandler = async (request, h) => {
     return response;
   }
 
-  const db = client.db('nama_database'); // Ganti dengan nama basis data Anda
-  const usersCollection = db.collection('users');
-
   // Mencari pengguna dengan nama pengguna yang diberikan
-  const user = await usersCollection.findOne({ username });
+  const user = await User.findOne({ username });
 
-  if (user) {
-    // Periksa kecocokan kata sandi
-    if (password === user.password) {
-      // Kata sandi cocok, proses login berhasil
-      const response = h.response({
-        status: 'success',
-        message: 'Login berhasil',
-      }).code(200);
-      return response;
-    }
+  if (user === null) {
+    // Tidak ada pengguna dengan nama pengguna yang diberikan
+    const response = h.response({
+      status: 'fail',
+      message: 'Username  salah',
+    }).code(401);
+    return response;
+  }
+
+  if (password === user.password) {
+    // Kata sandi cocok, proses login berhasil
+    const response = h.response({
+      status: 'success',
+      message: 'Login berhasil',
+    }).code(200);
+    return response;
   }
 
   // Jika tidak ada pengguna atau kata sandi tidak cocok
   const response = h.response({
     status: 'fail',
-    message: 'Username atau password salah',
+    message: 'Password salah',
   }).code(401);
   return response;
 };
--- struktur object classes.md
+++ struktur object classes.md
@@ -28,4 +28,8 @@ curl -X POST -H "Content-Type: application/json" -d "{\"nim\":\"21106050048\",\"
 
 {"status":"success","message":"Jadwal Kelas berhasil ditambahkan","data":{"classCode":"Kode Kelas Metode Pengembangan Perangkat Lunak A"}}
 
+username, password, email, nama, nim_nip, role,
+
+curl -X POST -H "Content-Type: application/json" -d "{\"username\":\"21106050048\",\"password\":\"21106050048\",\"email\":\"21106050048@student.uin-suka.ac.id\",\"nama\":\"Nanda Arya Putra\",\"nim_nip\":\"21106050048\",\"role\":\"mahasiswa\"}" https://simak-back-end.cyclic.app/api/users/register
+
 keytool -genkey -v -keystore simak_keystore.jks -keyalg RSA -keysize 2048 -validity 10000 -alias nandaaryaputra
